13f4bd98dd10281141d18ea8ea3db16e5b369a55
HC1058503505 <1058503505@qq.com>
2018-09-11 18:09:58 +0800

    Test

---
+++ HCSwiftGuidePage/Classes/ReplaceMe.swift
@@ -0,0 +1,10 @@
+
+struct Test {
+   static var constant = 10
+    
+    func TestMe() {
+        print("TestMe")
+    }
+}
+
+
---
+++ TestSwift.swift
@@ -0,0 +1,14 @@
+//
+//  TestSwift.swift
+//  Pods-HCSwiftGuidePage_Example
+//
+//  Created by cgtn on 2018/9/11.
+//
+
+import UIKit
+
+class TestSwift {
+    func TestMe() {
+        print("Test Me")
+    }
+}
